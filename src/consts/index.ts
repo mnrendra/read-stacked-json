dfb71a7281fd8bc41c3d492b5595f697a413b068
Muhammad Rendra <mnrendra@users.noreply.github.com>
2024-06-18 16:15:34 +0800

    feat: windows compatibility (#18)
    issues:
    * feat: windows compatibility (#17)
    
    commits:
    * feat: windows compatibility (10d58d9)

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,2 +1,5 @@
+import { normalize } from 'node:path'
+
 export const EXT = '.json'
-export const SKIPPED_STACK = 'node_modules/@mnrendra/read-stacked-json'
+export const SKIPPED_STACK =
+  normalize('node_modules/@mnrendra/read-stacked-json')
