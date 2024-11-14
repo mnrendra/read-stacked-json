b1e7105c5bb2e6713b63367e275a237d47eb69ba
Muhammad Rendra <mnrendra@users.noreply.github.com>
2024-11-14 21:45:48 +0700

    fix: cwd

--- src/types/JSONType.ts
+++ src/types/JSONType.ts
@@ -1,3 +1,8 @@
+/**
+ * `JSON` type (an alias for `Record<string, any>`).
+ *
+ * @see https://github.com/mnrendra/read-stacked-json#readme
+ */
 type JSONType = Record<string, any>
 
 export default JSONType
